9cb11c074b638e46644dc34c134d51b89724bb18
kabaskill <oguzkabasakal@gmail.com>
2026-05-12 22:13:51 +0200

    asteroids: asteroids and collision logic

--- asteroids-python/main.py
+++ asteroids-python/main.py
@@ -1,6 +1,10 @@
+import sys
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_event, log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,14 +15,40 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+
+        for drawables in drawable:
+            drawables.draw(screen)
+
+        updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         pygame.display.flip()
-        clock.tick(60)
+        # limit to 60fps
         dt = clock.tick(60) / 1000
 
 
