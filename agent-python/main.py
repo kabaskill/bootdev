ef2370f109eb8bda2c6bcd79d80183cb92b7a65f
kabaskill <oguzkabasakal@gmail.com>
2026-05-15 22:54:37 +0200

    ai-agent: add verbose output

--- agent-python/main.py
+++ agent-python/main.py
@@ -1,28 +1,40 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
-import argparse
+from google.genai import types
 
 
 def main():
-    load_dotenv()
-    api_key=os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-
     parser = argparse.ArgumentParser(description='AI Agent')
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-    response= client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    load_dotenv()
+    api_key=os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages
+    )
+
     text = response.text
     metadata = response.usage_metadata
 
+    if args.verbose:
+        print("User prompt: ", args.user_prompt)
 
-    if metadata is not None:
-        print("Prompt tokens: ", metadata.prompt_token_count)
-        print("Response tokens: ", metadata.candidates_token_count)
+        if metadata is not None:
+            print("Prompt tokens: ", metadata.prompt_token_count)
+            print("Response tokens: ", metadata.candidates_token_count)
 
-    print(text)
+    print("Response: \n", text)
 
 
 if __name__ == "__main__":
