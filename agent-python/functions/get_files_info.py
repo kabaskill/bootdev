97b3f30299867fb46a59bfdcb54b823424e53659
kabaskill <oguzkabasakal@gmail.com>
2026-05-16 15:25:23 +0200

    ai-agent: add get_files_info function

---
+++ agent-python/functions/get_files_info.py
@@ -0,0 +1,28 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+        if os.path.commonpath([abs_working_dir, target_dir]) != abs_working_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        display_dir = directory if directory != "." else "current"
+
+        files_info = []
+        print(f"Result for '{display_dir}' directory:")
+        for file in os.listdir(target_dir):
+                full_path = os.path.join(target_dir, file)
+                size = os.path.getsize(full_path)
+                is_directory = os.path.isdir(full_path)
+                files_info.append(f"- {file}: file_size={size}, is_dir={is_directory}")
+        return "\n".join(files_info)
+
+    except Exception as e:
+        return f"Error listing files: {e}"
+
+
+
