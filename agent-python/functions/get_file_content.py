243895298c0010c3627e2614c2273bc3a33a509d
kabaskill <oguzkabasakal@gmail.com>
2026-05-17 15:02:23 +0200

    ai-agent: add response loop and finish the project

--- agent-python/functions/get_file_content.py
+++ agent-python/functions/get_file_content.py
@@ -1,6 +1,9 @@
 import os
 
-from config import MAX_CHARS
+from google.genai import types
+
+MAX_CHARS = 10000
+
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
@@ -17,11 +20,27 @@ def get_file_content(working_directory, file_path):
             content = f.read(MAX_CHARS)
 
             if f.read(1):
-                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
 
     except Exception as e:
         return f'Error: reading file "{file_path}": {e}'
 
     return content
- 
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Retrieves the content (at most {MAX_CHARS} characters) of a specified file within the working directory",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read, relative to the working directory",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
